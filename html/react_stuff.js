248c3977d43e7c7a8483f33362a71b08fba52edd
Maarten de Jonge <mdejonge1990@gmail.com>
2018-12-05 02:31:19 +0100

    implement very basic UI in React
    Only lists current repositories and offers a button to create a new one.

--- html/react_stuff.js
+++ html/react_stuff.js
@@ -2,24 +2,95 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class CreateRepository extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {name: "", msg: ""}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const name = this.state.name;
+    var formData = new FormData();
+    formData.append("name", name);
+    fetch("http://localhost:3000/create_repository", {
+      method: "POST",
+      body: formData
+    })
+      .then(response => response.json())
+      .then(response => this.setState(
+        {msg: response.data.success ? "Succesfully created repository." : "Could not create repository."})
+      ),
+
+      error => alert("Error!");
+
+    event.preventDefault();
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    const {name, msg} = this.state;
+    return e("form", {onSubmit: this.handleSubmit},
+             e("label", null, "Name: ",
+               e("input", {type: "text", value: name, onChange: this.handleChange})),
+             e("input", {type: "submit", value: "Create new"}),
+             e("label", null, msg));
   }
 }
 
-const domContainer = document.querySelector('#react_stuff');
-ReactDOM.render(e(LikeButton), domContainer);
+class RepoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      error: null,
+      names: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/get_repositories")
+      .then(res => res.json())
+      .then(result => {
+        this.setState( {
+          isLoaded: true,
+          names: result.data
+        });
+      }),
+
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+  }
+
+  render() {
+    const { isLoaded, error, names } = this.state;
+    var list;
+    if (error) {
+      list = e("div", null, error.message);
+    } else if (!isLoaded) {
+      list = e("div", null, "Loading...");
+    } else {
+      list = e(
+        "ul",
+        null,
+        names.map(name => e("li", {key: name}, name))
+      );
+    }
+
+    return e("div", null,
+             list,
+             e(CreateRepository));
+  }
+}
+
+const domContainer = document.querySelector('#repolist');
+ReactDOM.render(e(RepoList), domContainer);
