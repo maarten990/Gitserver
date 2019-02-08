38c6761798ed4d2c1371f56171f3a2d1b663b737
Maarten de Jonge <mdejonge1990@gmail.com>
2019-02-08 16:46:22 +0100

    remove unused import

--- html-client/src/FileViewContainer.js
+++ html-client/src/FileViewContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFile, faFolder } from '@fortawesome/free-solid-svg-icons'
 import Highlight from 'react-highlight'
