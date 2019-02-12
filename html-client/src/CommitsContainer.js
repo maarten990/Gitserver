780d200ac4d32f684fb81187c5da895206ecacda
Maarten de Jonge <mdejonge1990@gmail.com>
2019-02-12 16:50:34 +0100

    split commit messages into a summary and a body

--- html-client/src/CommitsContainer.js
+++ html-client/src/CommitsContainer.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUndo } from '@fortawesome/free-solid-svg-icons'
 import ListPlaceholder from './ListPlaceholder'
 import { apiCall, usePrevious } from './util.js'
 
-const Commit = ({ repoName, message, sha1 }) => (
+const MessageView = ({ message, onClose }) => (
+  <div className='message-view'>
+    <button onClick={onClose}>
+      <FontAwesomeIcon icon={faUndo} />
+    </button>
+    {message.split(/\n/).map((line, i) => <p className='monospace' key={i}>{line}</p>)}
+  </div>
+)
+
+const Commit = ({ repoName, summary, body, sha1, onClick }) => (
   <>
     <tr>
       <td className='commit-message monospace'>
-        <Link to={`/repo/${repoName}/${sha1}`}>{message}</Link>
+        <Link to={`/repo/${repoName}/${sha1}`} onClick={e => {onClick(summary, body)}}>{summary}</Link>
       </td>
     </tr>
     <tr>
@@ -18,12 +29,12 @@ const Commit = ({ repoName, message, sha1 }) => (
   </>
 )
 
-const CommitList = ({ repoName, commits, isLoaded }) => {
+const CommitList = ({ repoName, commits, isLoaded, onClick }) => {
   if (isLoaded) {
     return (
       <table className="commit-list">
         <tbody>
-          {commits.map((c, i) => <Commit repoName={repoName} message={c.message} sha1={c.sha1} key={i} />)}
+          {commits.map((c, i) => <Commit repoName={repoName} summary={c.summary} body={c.body} sha1={c.sha1} onClick={onClick} key={i} />)}
         </tbody>
       </table>
     )
@@ -34,12 +45,22 @@ const CommitList = ({ repoName, commits, isLoaded }) => {
   }
 }
 
+// convert a commit's message to an object with a summary and a body
+const summarizeCommit = ({ message, sha1 }) => {
+  let lines = message.split(/\n/)
+  return {
+    summary: lines[0],
+    body: lines.slice(1).join('\n'),
+    sha1: sha1
+  }
+}
+
 const getCommits = (name, setCommits, setLoaded) => {
   apiCall('get_commits', { name: name })
     .then(response => response.data)
     .catch(() => [])
     .then(commits => {
-      setCommits(commits)
+      setCommits(commits.map(summarizeCommit))
       setLoaded(true)
     })
 }
@@ -47,6 +68,7 @@ const getCommits = (name, setCommits, setLoaded) => {
 const CommitsContainer = ({ name }) => {
   const [loaded, setLoaded] = useState(false)
   const [commits, setCommits] = useState([])
+  const [activeCommit, setActiveCommit] = useState(null)
   const prevRepoName = usePrevious(name);
 
   useEffect(() => {
@@ -63,13 +85,23 @@ const CommitsContainer = ({ name }) => {
     getCommits(name, setCommits, setLoaded)
   }, [loaded])
 
-  return (
-    <div className="commits-container">
-      <h1>Commits</h1>
+  let contents
+  if (activeCommit) {
+    contents =  <MessageView message={activeCommit} onClose={() => setActiveCommit(null)} />
+  } else {
+    contents = (
       <CommitList
         repoName={name}
         commits={commits}
-        isLoaded={loaded} />
+        isLoaded={loaded}
+        onClick={(summary, body) => setActiveCommit(`${summary}\n\n${body}`)} />
+    )
+  }
+
+  return (
+    <div className="commits-container">
+      <h1>Commits</h1>
+      {contents}
     </div>
   )
 }
